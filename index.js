881966b2066e1313db3b67b5841d9aa2e98ae90a
pnlybubbles <pnlybubbles@gmail.com>
2018-06-19 11:46:35 +0900

    リンクをクリックした時のアニメーションを追加

---
+++ index.js
@@ -0,0 +1,38 @@
+// ページ内移動をするリンクの取得
+const links = document.querySelectorAll('a[href^="#"]')
+
+// 取得したリンクに対して
+links.forEach(link => {
+  // リンク先の要素の取得
+  const targetId = link.getAttribute('href').slice(1)
+  const target = document.getElementById(targetId)
+  // 無かったら中断
+  if (!target) { return }
+  // リンクがクリックされた時
+  link.addEventListener('click', e => {
+    // リンクをクリックした時のデフォルトの挙動を無効化
+    e.preventDefault()
+    // スクロールアニメーションする
+    scrollTo(target, -30)
+  })
+})
+
+/**
+ * targetまでアニメーションしながらスクロールする
+ * offsetを指定するとその分だけ多めに下にスクロールする
+ * @param {HTMLElement} target 
+ * @param {number} offset 
+ */
+const scrollTo = (target, offset) => {
+  anime({
+    targets: { scroll: document.documentElement.scrollTop },
+    scroll: target.offsetTop + offset,
+    // アニメーション時間 (ミリ秒)
+    duration: 400,
+    // アニメーションの動きの種類 (http://animejs.com/documentation/#penner)
+    easing: 'easeInOutQuart',
+    run: (a) => {
+      document.documentElement.scrollTop = a.animations[0].currentValue
+    },
+  })
+}
